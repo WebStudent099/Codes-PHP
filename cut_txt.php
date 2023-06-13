3cd2b8bbe1ad99291e3325db1884fd4736222913
WebStudent099 <135273949+WebStudent099@users.noreply.github.com>
2023-06-13 17:00:50 +0100

    Update cut_txt.php

--- cut_txt.php
+++ cut_txt.php
@@ -1,1 +1,29 @@
+<!DOCTYPE html>
+<html>
+<body>
+	
+<?php
+	$texte = "sault - ok - je suis bon - et toi ?";
+	
+	//fist way
+	$array1 = preg_split("/[-]/",$texte);
+	//à l´interrieur des crochets on peut mettre plusieurs caractères
+	//si l´on souhaite decouper la chaine en fonction de plusiers caractères.
+	//Exemple pour _,\ et @ on a [_\@]
+	foreach ($array1 as $arr1){
+		echo $arr1."<br>";
+	}
+
+	//second way
+	$array2 = explode("-",$texte); 
+	//permet de couper uniquement en fonction 
+	//d´un seul caractère
+	foreach($array2 as $arr2){
+		echo "<br>".$arr2;
+	}
+
+?> 
+
+</body>
+</html>
 
